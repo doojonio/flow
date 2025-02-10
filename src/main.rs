7845379ef94739a384771d81bb2613b4b07490a9
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2025-02-10 20:37:09 +0500

    feat: create tasks

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,55 @@
-use iced::widget::{button, text, column, Column};
+use iced::widget::{button, column, text, text_input, Column};
+
+fn main() -> iced::Result {
+    iced::application("Flow. Saga. Sunrise.", Flow::update, Flow::view)
+        .window_size((300.0, 500.0))
+        .centered()
+        .run()
+    // iced::run("Flow. Saga. Sunrise.", Flow::update, Flow::view)
+}
+
+struct Task {
+    name: String,
+}
 
 #[derive(Default)]
-struct Counter {
-    value: i32,
+struct Flow {
+    input_take_name: String,
+    tasks: Vec<Task>,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum Message {
-    Increment,
-    Decrement,
+    CreateTask,
+    InputChanged(String),
 }
 
-impl Counter {
+impl Flow {
     fn update(&mut self, message: Message) {
         match message {
-            Message::Increment => {
-                self.value += 1;
-            },
-            Message::Decrement => {
-                self.value -= 1;
+            Message::CreateTask => {
+                self.tasks.push(Task {
+                    name: self.input_take_name.clone(),
+                });
+                self.input_take_name.clear();
+            }
+            Message::InputChanged(cont) => {
+                self.input_take_name = cont;
             }
         }
     }
 
     fn view(&self) -> Column<Message> {
-        column![
-            button("+").on_press(Message::Increment),
-            text(self.value),
-            button("-").on_press(Message::Decrement),
-        ]
-    }
-}
+        let col = column![
+            text_input("Task name", &self.input_take_name).on_input(Message::InputChanged),
+            button("Create").on_press(Message::CreateTask)
+        ];
 
-fn main() -> iced::Result {
-    iced::run("Flow. Saga. Sunrise.", Counter::update, Counter::view)
+        col.extend(
+            self.tasks
+                .iter()
+                .map(|t| iced::Element::from(text(&t.name)))
+                .into_iter(),
+        )
+    }
 }
