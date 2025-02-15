6edf4ab80d7a440ad931666d9ffc8692b67983e5
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2025-02-15 18:10:40 +0500

    feat: save n load state

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,140 @@
-use bincode::{self};
-use iced::widget::{button, column, text, text_input, Column};
+use iced::widget::{button, checkbox, column, keyed_column, row, text_input, Column, Row};
 use serde::{Deserialize, Serialize};
-use std::fs;
 
-fn main() -> iced::Result {
-    iced::application("Flow. Saga. Sunrise.", Flow::update, Flow::view)
-        .window_size((300.0, 500.0))
+fn main() {
+    iced::application(Flow::title, Flow::update, Flow::view)
         .centered()
-        .run()
-    // iced::run("Flow. Saga. Sunrise.", Flow::update, Flow::view)
+        .run_with(Flow::new)
+        .unwrap()
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 struct Task {
-    name: String,
+    content: String,
+}
+
+impl Task {
+    fn get_title(&self) -> String {
+        self.content.clone()
+    }
+
+    fn view(&self) -> Row<Message> {
+        row![checkbox(&self.content, false)]
+    }
 }
 
 #[derive(Default)]
 struct Flow {
-    input_take_name: String,
-    tasks: Vec<Task>,
+    task_input_content: String,
+    state: State,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
-    CreateTask,
+    CreateTaskClicked,
+    StateSaved(Result<(), &'static str>),
+    StateLoaded(Result<State, &'static str>),
     InputChanged(String),
 }
 
 //bincode
 
+const DATA_PATH: &'static str = "./state.bin";
+
 impl Flow {
-    fn update(&mut self, message: Message) {
-        let enc = fs::read("tasks.bin");
-        if enc.is_ok() {
-            let (decoded, _bytes_read): (Vec<Task>, usize) =
-                bincode::serde::decode_from_slice(&enc.unwrap(), bincode::config::standard())
-                    .unwrap();
-            self.tasks = decoded;
-        }
+    fn new() -> (Self, iced::Task<Message>) {
+        (
+            Flow::default(),
+            iced::Task::perform(State::load(String::from(DATA_PATH)), Message::StateLoaded),
+        )
+    }
 
+    fn title(&self) -> String {
+        String::from("Flow")
+    }
+
+    fn update(&mut self, message: Message) -> iced::Task<Message> {
         match message {
-            Message::CreateTask => {
-                self.tasks.push(Task {
-                    name: self.input_take_name.clone(),
-                });
-                self.input_take_name.clear();
+            Message::StateLoaded(load_r) => {
+                self.state = load_r.unwrap();
+                println!("Tasks: {:?}", self.state.tasks);
+            }
+            Message::InputChanged(content) => self.task_input_content = content,
+            Message::CreateTaskClicked => {
+                if self.task_input_content.len() > 0 {
+                    self.state.add_task(self.task_input_content.clone());
+                    self.task_input_content = String::new();
 
-                let result =
-                    bincode::serde::encode_to_vec(&self.tasks, bincode::config::standard())
-                        .unwrap();
-                fs::write("tasks.bin", &result).expect("failed tro write");
+                    return iced::Task::perform(
+                        self.state.clone().save(String::from(DATA_PATH)),
+                        Message::StateSaved,
+                    );
+                }
             }
-            Message::InputChanged(cont) => {
-                self.input_take_name = cont;
-            }
+            Message::StateSaved(result) => match result {
+                Ok(()) => println!("saved ok"),
+                Err(msg) => println!("saved not ok {}", msg),
+            },
+            // _ => {}
         }
+
+        iced::Task::none()
     }
 
     fn view(&self) -> Column<Message> {
-        let col = column![
-            text_input("Task name", &self.input_take_name).on_input(Message::InputChanged),
-            button("Create").on_press(Message::CreateTask)
-        ];
+        column![
+            text_input("Task name", &self.task_input_content).on_input(Message::InputChanged),
+            button("Create").on_press(Message::CreateTaskClicked),
+            column(self.state.tasks.iter().map(|t| t.view().into()))
+        ]
+    }
+}
 
-        col.extend(
-            self.tasks
-                .iter()
-                .map(|t| iced::Element::from(text(&t.name)))
-                .into_iter(),
-        )
+#[derive(Default, Debug, Clone)]
+struct State {
+    tasks: Vec<Task>,
+}
+
+impl State {
+    fn add_task(&mut self, content: String) -> String {
+        let task = Task { content: content };
+        let title = task.get_title();
+        self.tasks.push(task);
+
+        return String::from(title);
+    }
+
+    async fn save(self, path: String) -> Result<(), &'static str> {
+        use async_std::prelude::*;
+
+        let mut file = async_std::fs::File::create(path)
+            .await
+            .map_err(|_| "failed create")?;
+
+        let buf = bincode::serde::encode_to_vec(&self.tasks, bincode::config::standard())
+            .map_err(|_| "failed encode")?;
+        file.write_all(&buf).await.map_err(|_| "failed write")?;
+
+        Ok(())
+    }
+
+    async fn load(path: String) -> Result<State, &'static str> {
+        use async_std::prelude::*;
+
+        let mut file = async_std::fs::File::open(path)
+            .await
+            .map_err(|_| "open create")?;
+
+        let mut buf = Vec::new();
+
+        file.read_to_end(&mut buf)
+            .await
+            .map_err(|_| "failed read")?;
+
+        let (tasks, _): (Vec<Task>, usize) =
+            bincode::serde::decode_from_slice(&buf, bincode::config::standard())
+                .map_err(|_| "failed to decode")?;
+
+        Ok(State { tasks: tasks })
     }
 }
